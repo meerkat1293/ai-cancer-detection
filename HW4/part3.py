e13ac13f597b1a5a945841ed52088afd805c0aea
meerkat1293 <meermatthew1293@gmail.com>
2019-10-27 21:02:00 -0700

    HW4

---
+++ HW4/part3.py
@@ -0,0 +1,12 @@
+
+def has_consecutive(some_string):
+    place_holder = ""
+    for i in some_string:
+        if i == place_holder:
+            return True
+        elif i != place_holder:
+            place_holder = i
+            continue
+    return False
+
+print(has_consecutive("abccd"))
