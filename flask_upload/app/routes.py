cf85cbf71b1d7cae5a7f4de209d5f351f998f2b7
Danial Khan <danialk1@berkeley.edu>
2020-06-13 22:28:45 -0700

    review flask to prove legitimacy

--- flask_upload/app/routes.py
+++ flask_upload/app/routes.py
@@ -9,7 +9,7 @@ ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'csv'])
 
 def allowed_file(filename):
 	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-	
+
 @app.route('/')
 def upload_form():
 	return render_template('upload.html')
@@ -42,3 +42,4 @@ def upload_file():
 
 if __name__ == "__main__":
     app.run()
+	
